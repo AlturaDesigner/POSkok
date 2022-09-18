60a5cc21f7ca135a33d4bfaa4f50c6bf090b048a
Designer <56726555+AlturaDesigner@users.noreply.github.com>
2022-09-18 22:48:32 +0200

    Deleting OLD

--- Frontend/components/header.js
+++
@@ -1,9 +0,0 @@
-    //header.js will hold the header section.
-    function Header() {
-      return (
-        <div className="header">
-          <h2>ToDo app</h2>
-        </div>
-      );
-    }
-    export default Header;
