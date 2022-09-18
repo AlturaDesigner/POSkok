707d3b0c548dee11f841dc8b151b9d5b25eabe03
Designer <56726555+AlturaDesigner@users.noreply.github.com>
2022-09-18 21:01:27 +0200

    Testing commit error

--- Frontend/.next/static/chunks/webpack.js
+++ Frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3d2c66776812e334"; }
+/******/ 		__webpack_require__.h = function() { return "14ab4f471df38141"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
