d097a3231c3c46af3aa75d54a0cdbae0ccad0f73
Designer <56726555+AlturaDesigner@users.noreply.github.com>
2022-09-19 09:26:20 +0200

    gitignore update,

--- Frontend/.next/static/chunks/webpack.js
+++ Frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "14ab4f471df38141"; }
+/******/ 		__webpack_require__.h = function() { return "12b1c9315216ff31"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
